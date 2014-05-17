ac83a231b393ab8212b76a2887991cd128d48345
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-05-17 23:14:09 +0200

    Make tests pass on Django 1.7 without warnings.

--- contact/test_settings.py
+++ contact/test_settings.py
@@ -8,10 +8,14 @@ DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'}}
 
 INSTALLED_APPS = ['contact', 'django.contrib.staticfiles']
 
+MIDDLEWARE_CLASSES = []
+
 ROOT_URLCONF = 'contact.test_urls'
 
 SECRET_KEY = 'whatever'
 
 STATIC_URL = '/static/'
+
+TEMPLATE_CONTEXT_PROCESSORS = ['django.core.context_processors.static']
 
 TEMPLATE_DIRS = [os.path.join(os.path.dirname(__file__), 'test_templates')]
