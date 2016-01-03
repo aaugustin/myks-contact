225abbf06472fe7afd15252ca446456c4caed0bb
Aymeric Augustin <aymeric.augustin@m4x.org>
2016-01-03 16:28:05 +0100

    Update test settings for Django >= 1.8.

--- contact/test_settings.py
+++ contact/test_settings.py
@@ -8,14 +8,21 @@ DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'}}
 
 INSTALLED_APPS = ['contact', 'django.contrib.staticfiles']
 
-MIDDLEWARE_CLASSES = []
-
 ROOT_URLCONF = 'contact.test_urls'
 
 SECRET_KEY = 'whatever'
 
 STATIC_URL = '/static/'
 
-TEMPLATE_CONTEXT_PROCESSORS = ['django.core.context_processors.static']
-
-TEMPLATE_DIRS = [os.path.join(os.path.dirname(__file__), 'test_templates')]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(os.path.dirname(__file__), 'test_templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.static',
+            ],
+        },
+    },
+]
